438d33c8eaa77d5537b92f6a76192236df484505
Gamaliel Tellez <gamatellez@gmail.com>
2015-01-29 16:29:13 -0700

    Adds label to the main view

---
+++ pre-course-git/pre-course-git/pre-course-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by Gamaliel Tellez on 1/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
