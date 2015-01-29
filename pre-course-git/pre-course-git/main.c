735bd23ce57134a6cea4504609f386837bb89f1d
Gamaliel Tellez <gamatellez@gmail.com>
2015-01-29 16:03:27 -0700

    first commit

---
+++ pre-course-git/pre-course-git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pre-course-git
+//
+//  Created by Gamaliel Tellez on 1/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
